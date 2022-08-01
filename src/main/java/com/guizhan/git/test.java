c3e0f3077b89bec14db93e0a30b1a9fc2d20298c
Arche <596121893@qq.com>
2022-08-01 21:56:07 +0800

    master commit2

--- src/main/java/com/guizhan/git/test.java
+++ src/main/java/com/guizhan/git/test.java
@@ -6,5 +6,6 @@ public class test {
         System.out.println("hello1");
         System.out.println("hello2");
         System.out.println("hello3");
+        System.out.println("master test");
     }
 }
